82232c7df0f34b5128a168a60623552a0b0f36b2
Anish M V <anishmv@kofukuidealabs.com>
2023-11-20 01:34:18 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      dark: "#030303",
+      white: "#FFFFFF",
+      purple: "#6741D9",
+      pink: "#F0C3F1",
+      greyed: "#C2C2C2",
+    },
+    fontSize: {
+      Heading: "32px",
+    },
+    fontFamily: {
+      display: ["Poppins"],
+    },
+    extend: {},
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+};
